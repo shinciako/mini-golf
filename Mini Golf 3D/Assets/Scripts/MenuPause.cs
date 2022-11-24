f21391573c48acaeb0025fb7671c9cbc21337cf0
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-24 20:12:16 +0100

    MenuPause

---
+++ Mini Golf 3D/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    static bool isPaused = false;
+    public GameObject menu;
+
+    public void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                resume();
+            }else{
+                menu.SetActive(true);
+                Time.timeScale = 0f;
+                isPaused = true;
+            }
+        }
+    }
+
+    public void resume(){
+        menu.SetActive(false);
+                Time.timeScale = 1f;
+                isPaused = false;
+    }
+
+    public void goMenu(){
+    SceneManager.LoadScene(0);
+   }
+}
